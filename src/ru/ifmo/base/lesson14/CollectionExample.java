991c4db7190c746ac4083a83d6e26e6ead9abda3
dasha <frgyvu@gmail.com>
2020-04-20 21:57:47 +0300

    collection lesson++

--- src/ru/ifmo/base/lesson14/CollectionExample.java
+++ src/ru/ifmo/base/lesson14/CollectionExample.java
@@ -1,7 +1,7 @@
 package ru.ifmo.base.lesson14;
 
 
-import java.util.LinkedList;
+import java.util.*;
 
 public class CollectionExample {
     public static void main(String[] args) {
@@ -26,6 +26,7 @@ public class CollectionExample {
 
         System.out.println("---List---");
         System.out.println("---LinkedList---");
+        // двунаправленный связный список
         // Особенности LinkedList:
         // 1. элементы хранятся в порядке добавления
         // 2. можно хранить null
@@ -36,9 +37,143 @@ public class CollectionExample {
         studentLinkedList.add(student2); // добавление элемента в конец списка
         studentLinkedList.add(student1); // добавление элемента в конец списка
 
+        // добавление элемента на указанную позицию (индекс первого
+        // элемента - 0)
+        studentLinkedList.add(1, student3);
+        // при обращении к несуществующему индексу:
+        // IndexOutOfBoundsException
+//        studentLinkedList.add(10, student3);
 
+        studentLinkedList.addFirst(null); // добавление элемента
+        // в начало списка
+
+        System.out.println(studentLinkedList);
+
+        // для каждого элемента коллекции будет вызван метод
+        // System.out.println()
+        studentLinkedList.forEach(System.out::println);
+
+        System.out.println(studentLinkedList.getLast()); // вернет последний
+        System.out.println(studentLinkedList.getFirst()); // вернет 1й
+        System.out.println(studentLinkedList.get(2)); // вернет элемент,
+        // находящий на указанной позиции
+
+        studentLinkedList.removeFirst(); // удаление первого элемента
+        studentLinkedList.removeLast(); // удаление последнего элемента
+        studentLinkedList.remove(1); // удаление элемента по индексу
+        studentLinkedList.remove(student1); // удаляет переданный
+        // элемент из списка
+        studentLinkedList.remove(); // == removeFirst();
+
+        System.out.println("после удаления " + studentLinkedList);
 
         System.out.println("---ArrayList---");
+        // динамически расширяемый массив
+        // Особенности ArrayList:
+        // 1. реализован на основе массива
+        // 2. порядок хранения элементов соответствуе порядку добавления
+        // 3. можно хранить null
+        // 4. допускает хранения дублирующихся элементов
+
+        // внутри ArrayList по умолчанию создается массив на 10 элементов
+        ArrayList<Student> studentArrayList = new ArrayList<>();
+        // можно указать изначальный размер ArrayList
+        studentArrayList = new ArrayList<>(20);
+        System.out.println(studentArrayList.size()); // количество элементов
+        // коллекции
+
+        studentArrayList.add(student1); // добавление в конец ArrayList
+        studentArrayList.add(student2); // добавление в конец ArrayList
+        studentArrayList.add(1, null); // добавление
+        // на указанную позицию
+
+        System.out.println(studentArrayList.size()); // 3
+
+        studentArrayList.trimToSize(); // урезает внутренний массив
+        // до количества элементов ArrayList
+
+        // можно получить часть списка, указав [начальную;конечную) позиции
+        System.out.println("sublist " + studentArrayList.subList(0, 1));
+
+        System.out.println(studentArrayList.get(0)); // получение по индексу
+
+        studentArrayList.remove(student1);
+        studentArrayList.remove(1);
+//        studentArrayList.removeAll(удаляемая коллекция);
+
+        Student[] studentsArray = {student1, student2, student3};
+        List<Student> arrAsList = Arrays.asList(studentsArray); // возвращает список
+        studentArrayList.addAll(arrAsList); // на вход необходимо передать
+        // коллекцию - Collection
+
+        System.out.println("---Интерфейс Set---");
+        // Особенности Set:
+        // 1. позволяет хранить только уникальные элементы
+        // 2. обязательно должны быть переопределены методы
+        // equals и hashCode
+
+        System.out.println("---HashSet---");
+        // Особенности HashSet:
+        // 1. основан на hash таблице (использует hashCode)
+        // 2. порядок хранения элементов может отличаться
+        // от порядка добавления
+        // 3. может хранить null (всегда на 1м месте)
+
+        HashSet<Student> studentHashSet = new HashSet<>(arrAsList);
+        System.out.println("studentHashSet " + studentHashSet);
+
+        System.out.println("---LinkedHashSet---");
+        // Особенности LinkedHashSet:
+        // 1. разница с HashSet:
+        // порядок хранения элементов не отличается от порядка добавления
+        LinkedHashSet<Student> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add(student1);
+        linkedHashSet.add(student2);
+        linkedHashSet.add(null);
+        System.out.println("linkedHashSet " + linkedHashSet);
+
+        System.out.println("---TreeSet---");
+        // Особенности TreeSet:
+        // 1. не позволяет добавить null
+        // 2. элементы хранит в отсортированном порядке
+        // 3. основан на алгоритме красно-черного дерева
+        // 4. необходимо задать порядок сортировки
+
+        // Для добавления элементов в TreeSet необходимо:
+        // 1. чтобы класс (экземпляры которого будут храниться в
+        // данной коллекции) реализовывал интерфейс Comparable
+        // в его метод compareTo (в нем определяется, какой объект считать
+        // большим, какой меньшим)
+        // 2. пердать в конструктор TreeSet компаратор - объект класса
+        // Comparator
+
+        // класс реализует интерфейс Comparable
+        TreeSet<Student> treeSet1 = new TreeSet<>();
+        treeSet1.add(student1);
+        treeSet1.add(student2);
+        treeSet1.add(student3);
+        System.out.println("treeSet1 " + treeSet1);
+
+        // пердать в конструктор TreeSet компаратор - объект класса Comparator
+        // сначала сортировка по имени, если имена одинаковые - по возрасту
+        Comparator<Student> studentComparator = new StudentNameComparator()
+                .thenComparing(new StudentAgeComparator());
+
+        TreeSet<Student> treeSet2 = new TreeSet<>(studentComparator);
+        treeSet2.add(student1);
+        treeSet2.add(student2);
+        treeSet2.add(student3);
+        System.out.println("treeSet2 " + treeSet2);
+
+
+
+
+
+
+
+
+
+
 
     }
 }
--- src/ru/ifmo/base/lesson14/Student.java
+++ src/ru/ifmo/base/lesson14/Student.java
@@ -1,6 +1,9 @@
 package ru.ifmo.base.lesson14;
 
-public class Student {
+
+import java.util.Comparator;
+
+public class Student implements Comparable<Student>{
     private String name;
     private String surname;
     private int age;
@@ -34,4 +37,56 @@ public class Student {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+
+        Student student = (Student) o;
+
+        if (age != student.age) return false;
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        return surname != null ? surname.equals(student.surname) : student.surname == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override // определяем, какой объект считать меньшим, какой большим
+    public int compareTo(Student o) {
+        return Integer.compare(getAge(), o.getAge());
+    }
 }
+
+class StudentNameComparator implements Comparator<Student> {
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
+
+class StudentAgeComparator implements Comparator<Student> {
+
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
+}
+
