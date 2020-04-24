b4355b71e74348c58474a99b2a9519c3d91fef22
dasha <frgyvu@gmail.com>
2020-04-24 14:03:31 +0300

    for reflection lesson

---
+++ src/ru/ifmo/base/lesson16/reflection/TextMessage.java
@@ -0,0 +1,20 @@
+package ru.ifmo.base.lesson16.reflection;
+
+import java.io.Serializable;
+
+public class TextMessage extends Message implements Serializable {
+    private String text;
+
+    public TextMessage(String title) {
+        super(title);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
