fdfa93fe6a8972730f328060c38a5afbc60c7b9f
dasha <frgyvu@gmail.com>
2020-05-06 13:47:18 +0300

    lesson19 serialization

---
+++ src/ru/ifmo/base/lesson19/serialization/Main.java
@@ -0,0 +1,26 @@
+package ru.ifmo.base.lesson19.serialization;
+
+public class Main  {
+    public static void main(String[] args) {
+
+        Pupil pupil1 = new Pupil();
+        pupil1.setAge(7);
+        pupil1.setName("pupil1");
+        pupil1.setGroup(new Group("Химия", 12));
+        pupil1.learn();
+
+        Pupil pupil2 = new Pupil();
+        pupil2.setAge(8);
+        pupil2.setName("pupil2");
+        pupil2.setGroup(new Group("Биология", 22));
+        pupil2.learn();
+
+        Director director = new Director("Super Speech");
+        director.setAge(44);
+        director.setName("director");
+        director.setRating(2);
+
+    }
+
+
+}
