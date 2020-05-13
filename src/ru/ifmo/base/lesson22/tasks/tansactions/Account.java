986375c39963d8d86c4fcf7db0e8731a3efe25ef
dasha <frgyvu@gmail.com>
2020-05-13 14:21:34 +0300

    for lesson 22 + tasks

---
+++ src/ru/ifmo/base/lesson22/tasks/tansactions/Account.java
@@ -0,0 +1,13 @@
+package ru.ifmo.base.lesson22.tasks.tansactions;
+
+public class Account {
+    private String number;
+    private long balance;
+
+    public Account(String number, long balance) {
+        this.number = number;
+        this.balance = balance;
+    }
+
+    // TODO: добавить геттеры и сеттеры
+}
