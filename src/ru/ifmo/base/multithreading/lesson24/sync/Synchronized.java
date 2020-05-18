719d974b25c6c5639176b99ce244bcd872c38e7c
dasha <frgyvu@gmail.com>
2020-05-18 15:27:35 +0300

    lesson 24

---
+++ src/ru/ifmo/base/multithreading/lesson24/sync/Synchronized.java
@@ -0,0 +1,53 @@
+package ru.ifmo.base.multithreading.lesson24.sync;
+
+import java.util.ArrayList;
+
+public class Synchronized {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        ArrayList<IncrementThread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            threads.add(new IncrementThread(counter));
+        }
+
+        for (IncrementThread thread: threads){
+            thread.start();
+        }
+
+        for (IncrementThread thread: threads){
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("counter = " + counter.getCounter());
+    }
+}
+
+class Counter {
+    private int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void increment(){
+        counter++;
+    }
+}
+
+class IncrementThread extends Thread {
+    private Counter counter;
+    public IncrementThread(Counter counter){
+        this.counter = counter;
+    }
+    @Override
+    public void run(){
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+}
